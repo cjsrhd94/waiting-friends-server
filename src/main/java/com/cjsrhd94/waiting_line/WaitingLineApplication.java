273c00671b7eee32cbb74bf5309d1370e5fe0ab6
cjsrhd94 <cjsrhd94@gmail.com>
2025-07-01 22:34:53 +0900

    init

---
+++ src/main/java/com/cjsrhd94/waiting_line/WaitingLineApplication.java
@@ -0,0 +1,13 @@
+package com.cjsrhd94.waiting_line;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitingLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitingLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cjsrhd94/waiting_line/WaitingLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjsrhd94.waiting_line;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
