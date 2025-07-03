6eebd8200e6f526be37cb0a7ce1c2e00a38a32f0
cjsrhd94 <cjsrhd94@gmail.com>
2025-07-03 13:01:25 +0900

    feat: user entity 구현

---
+++ src/main/java/com/cjsrhd94/waiting_line/domain/user/entity/User.java
@@ -0,0 +1,33 @@
+package com.cjsrhd94.waiting_line.domain.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    public User(String email, String password, String role) {
+        this.id = UUID.randomUUID();
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+}
