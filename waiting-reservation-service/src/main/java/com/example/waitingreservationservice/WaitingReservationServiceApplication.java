5923e9de44e46393fbd2c0381a49eced54f254ba
cjsrhd94 <cjsrhd94@gmail.com>
2025-07-16 16:24:00 +0900

    feat: msa 도입에 따른 reservation service 모듈 init

---
+++ waiting-reservation-service/src/main/java/com/example/waitingreservationservice/WaitingReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.waitingreservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitingReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitingReservationServiceApplication.class, args);
+	}
+
+}
---
+++ waiting-reservation-service/src/test/java/com/example/waitingreservationservice/WaitingReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waitingreservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
