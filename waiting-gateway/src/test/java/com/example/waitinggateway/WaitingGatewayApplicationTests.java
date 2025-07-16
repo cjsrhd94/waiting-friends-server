2401f7768a8097b224d2aa30393da91aa4e3dd07
cjsrhd94 <cjsrhd94@gmail.com>
2025-07-16 14:48:12 +0900

    feat: msa 도입에 따른 gateway 모듈 init

---
+++ waiting-gateway/src/test/java/com/example/waitinggateway/WaitingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waitinggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
