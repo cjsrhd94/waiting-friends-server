5ba5f3ce3fc81ba6568447db54ee938cdb5554b9
cjsrhd94 <cjsrhd94@gmail.com>
2025-08-06 22:03:47 +0900

    chore: yaml 파일내 gateway route 설정 config 클래스로 이관

---
+++ waiting-gateway/src/main/java/com/example/waitinggateway/config/FilterConfig.java
@@ -0,0 +1,71 @@
+package com.example.waitinggateway.config;
+
+import com.example.waitinggateway.filter.AuthorizationHeaderFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class FilterConfig {
+
+    private final AuthorizationHeaderFilter authorizationHeaderFilter;
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // user-service
+                .route("user-service",
+                        r -> r.path(
+                                        "/api/v1/users/login",
+                                        "/api/v1/users/sign-up",
+                                        "/api/v1/users/reissue",
+                                        "/api/v1/users/logout"
+                                )
+                                .filters(f -> f
+                                        .removeRequestHeader("Cookie")
+                                        .rewritePath("/user-service/(?<segment>.*)", "/${segment}")
+                                )
+                                .uri("lb://user-service")
+                )
+
+                .route("user-service",
+                        r -> r.path(
+                                        "/api/v1/users/**"
+                                )
+                                .filters(f -> f
+                                        .removeRequestHeader("Cookie")
+                                        .rewritePath("/user-service/(?<segment>.*)", "/${segment}")
+                                        .filter(authorizationHeaderFilter.apply(new AuthorizationHeaderFilter.Config()))
+                                )
+                                .uri("lb://user-service")
+                )
+
+                // reservation-service
+                .route("reservation-service",
+                        r -> r.path(
+                                        "/api/v1/reservations/**"
+                                )
+                                .filters(f -> f
+                                        .removeRequestHeader("Cookie")
+                                        .rewritePath("/reservation-service/(?<segment>.*)", "/${segment}")
+                                )
+                                .uri("lb://reservation-service")
+                )
+
+                // spot-service
+                .route("spot-service",
+                        r -> r.path(
+                                        "/api/v1/spots/**"
+                                )
+                                .filters(f -> f
+                                        .removeRequestHeader("Cookie")
+                                        .rewritePath("/spot-service/(?<segment>.*)", "/${segment}")
+                                )
+                                .uri("lb://spot-service")
+                )
+                .build();
+    }
+}
