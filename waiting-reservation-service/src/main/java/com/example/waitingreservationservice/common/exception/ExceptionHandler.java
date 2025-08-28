92ab7545f8f9317543676667da4991a025841c49
cjsrhd94 <cjsrhd94@gmail.com>
2025-08-29 06:58:13 +0900

    fix: exceptionHandler 내 RestControllerAdvice 어노테이션 추가

--- waiting-reservation-service/src/main/java/com/example/waitingreservationservice/common/exception/ExceptionHandler.java
+++ waiting-reservation-service/src/main/java/com/example/waitingreservationservice/common/exception/ExceptionHandler.java
@@ -4,6 +4,7 @@ import com.example.waitingcommon.exception.BusinessException;
 import com.example.waitingcommon.exception.GlobalErrorCode;
 import com.example.waitingcommon.response.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -11,17 +12,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-public class ReservationExceptionHandler {
+@Slf4j
+@RestControllerAdvice
+public class ExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     /**
      * API 호출 시 필수 parameter가 누락되었을 때 발생
      */
-    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<ErrorResponse> handleMissingServletRequestParameterException(
             HttpServletRequest request,
             MissingServletRequestParameterException e
@@ -41,7 +44,7 @@ public class ReservationExceptionHandler {
     /**
      * 유효한 parameter 값이 아닐 때 발생
      */
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorResponse> handleInvalidParameterException(
             HttpServletRequest request,
             MethodArgumentTypeMismatchException e
@@ -61,7 +64,7 @@ public class ReservationExceptionHandler {
     /**
      * API가 요구하는 URL 혹은 HTTP 메서드와 다를 떄 발생
      */
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<ErrorResponse> handleNoHandlerFoundException(HttpServletRequest request, HttpRequestMethodNotSupportedException e) {
         logException(request, e);
         return ResponseEntity
@@ -78,7 +81,7 @@ public class ReservationExceptionHandler {
     /**
      * API가 요구하는 content-type과 다를 떄 발생
      */
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     public ResponseEntity<ErrorResponse> handleHttpMediaTypeNotSupportedException(HttpServletRequest request, HttpMediaTypeNotSupportedException e) {
         logException(request, e);
         return ResponseEntity
@@ -96,7 +99,7 @@ public class ReservationExceptionHandler {
     /**
      * 비즈니스 로직에서 정의한 예외 발생
      */
-    @ExceptionHandler(BusinessException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(HttpServletRequest request, BusinessException e) {
         logException(request, e);
 
@@ -114,7 +117,7 @@ public class ReservationExceptionHandler {
     /**
      * 정의되지 않은 서버 오류 발생
      */
-    @ExceptionHandler(Exception.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleHttpMediaTypeNotSupportedException(HttpServletRequest request, Exception e) {
         logException(request, e);
         return ResponseEntity
