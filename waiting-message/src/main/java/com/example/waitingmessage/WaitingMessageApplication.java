9fb97f4d7b68dc3b524924601cbcbbed19818e80
cjsrhd94 <cjsrhd94@gmail.com>
2025-09-10 19:52:56 +0900

    waiting message init

---
+++ waiting-message/src/main/java/com/example/waitingmessage/WaitingMessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.waitingmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitingMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaitingMessageApplication.class, args);
+    }
+
+}
---
+++ waiting-message/src/test/java/com/example/waitingmessage/WaitingMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waitingmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
